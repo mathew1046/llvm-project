11d93fc416d6c06eaddea9dc80fd1bd8a590a117
Mathew Joseph <mathewjosephparakka@gmail.com>
2026-01-27 19:05:40 +0530

    Fix: changes test for llogbl to AllLongDouble

--- libc/test/shared/shared_math_test.cpp
+++ libc/test/shared/shared_math_test.cpp
@@ -116,7 +116,6 @@ TEST(LlvmLibcSharedMathTest, AllDouble) {
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::log1p(0.0));
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::log2(1.0));
   EXPECT_FP_EQ(0.0, LIBC_NAMESPACE::shared::sin(0.0));
-  EXPECT_EQ(0, LIBC_NAMESPACE::shared::llogbl(1.0L));
   EXPECT_EQ(0L, LIBC_NAMESPACE::shared::llogb(1.0));
   EXPECT_FP_EQ(0.0, LIBC_NAMESPACE::shared::tan(0.0));
 }
@@ -125,6 +124,7 @@ TEST(LlvmLibcSharedMathTest, AllLongDouble) {
   EXPECT_FP_EQ(0x0p+0L,
                LIBC_NAMESPACE::shared::dfmal(0x0.p+0L, 0x0.p+0L, 0x0.p+0L));
   EXPECT_EQ(0, LIBC_NAMESPACE::shared::ilogbl(0x1.p+0L));
+  EXPECT_EQ(0, LIBC_NAMESPACE::shared::llogbl(1.0L));
 }
 
 #ifdef LIBC_TYPES_HAS_FLOAT128
